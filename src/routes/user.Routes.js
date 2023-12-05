0130af3369f0456f277c86b401adc2d31fa79923
CyanMango377 <andr.sousasilva@outlook.com>
2023-12-05 18:52:16 +0000

    Public Route added

--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -9,5 +9,13 @@ const auth = require("../middlewares/auth");
 router.post("/user/login", log.logMiddleware, usersController.login);
 router.post("/user/register", log.logMiddleware, usersController.register);
 router.get("/user/:id", auth.required, log.logMiddleware, usersController.getUser);
+router.get("/", log.logMiddleware, (req, res) => {
+  try {
+    return res.status(200).json({ msg: "Welcome to our API" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Error on the Server! Try agian later!" });
+  }
+});
 
 module.exports = router;
